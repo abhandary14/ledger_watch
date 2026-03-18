066d8a079fc68d7d1a67f8b9a4f3c57d64ef2869
abhandary14 <aakashbhandary2024@gmail.com>
2026-03-18 02:07:42 -0400

    fix build errors

--- frontend/src/pages/app/DashboardPage.tsx
+++ frontend/src/pages/app/DashboardPage.tsx
@@ -291,7 +291,7 @@ export function DashboardPage() {
                     width={56}
                   />
                   <Tooltip
-                    formatter={(value: number) => [formatUSD(value), 'Spend']}
+                    formatter={(value) => [formatUSD(Number(value)), 'Spend']}
                     contentStyle={{ fontSize: 12 }}
                   />
                   <Line
